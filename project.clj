cc1e66f2fbb2af14906eafcf9c35309480c2db5e
broye <fqye@mashixiong.com>
2019-09-07 09:54:14 +0800

    Initial

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-bulma "0.1.0-SNAPSHOT"
+  :description "A bulma CSS framework component generator built with hiccup in Clojure"
+  :url "https://github.com/openqa-io/clojure-bulma"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [hiccup "1.0.5"]]
+  :repl-options {:init-ns clojure-bulma.core})
